66638608c8510af8bb4025110a8889bf68084cf2
Elliott <itali43@gmail.com>
2024-10-19 17:45:32 -0500

    about page fixed

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,17 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  transpilePackages: [
+    "antd",
+    "@ant-design",
+    "rc-util",
+    "rc-pagination",
+    "rc-picker",
+    "rc-notification",
+    "rc-tooltip",
+    "rc-tree",
+    "rc-table",
+  ],
 };
 
 export default nextConfig;
